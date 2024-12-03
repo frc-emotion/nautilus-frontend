25ce00bea8094f38fe6b217a2caad2afc3daef99
Arshan <111618520+ArshansGithub@users.noreply.github.com>
2024-12-02 19:45:04 -0800

    it actually compiles

--- app.config.js
+++ app.config.js
@@ -1,18 +1,21 @@
 const withCustomBeaconModule = require('./withCustomBeaconModule');
+const withRemoveiOSNotificationEntitlement = require('./withRemoveiOSNotificationEntitlement');
 
 export default ({ config }) => ({
   ...config,
     extra: {
       APP_UUID: "35a2ba12-25ba-4c79-9d0b-a88fc8d4255f",
       environment: "development",
-      API_URL: "http://staging.team2658.org:7001",
+      API_URL: "https://staging.team2658.org",
       MAX_RETRIES: 3,
       eas: {
         projectId: "9a4e6a54-63ce-46b8-a978-ba046e0f2ae8"
       }
     },
     plugins: [
-      ...(config.plugins || []),
       withCustomBeaconModule,
+      [withRemoveiOSNotificationEntitlement],
+      ...(config.plugins || [])
+      
     ],
   });
