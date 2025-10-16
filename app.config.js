ff502336e2c31b81ebb143f603d418050b8cd672
Mugilan Rajaraman <mugilanrajaraman@Mac.attlocal.net>
2025-10-15 19:34:46 -0700

    App.config.js pls

--- app.config.js
+++ app.config.js
@@ -3,22 +3,41 @@ const withRemoveiOSNotificationEntitlement = require('./withRemoveiOSNotificatio
 
 export default ({ config }) => ({
   ...config,
-    extra: {
-      APP_UUID: "35a2ba12-25ba-4c79-9d0b-a88fc8d4255f",
-      environment: "prod",
-      API_URL: "https://staging.team2658.org",
-      // API_URL: "http://0.0.0.0:7001/",
-      // API_URL: "https://api.team2658.org",
+  extra: {
+    APP_UUID: "35a2ba12-25ba-4c79-9d0b-a88fc8d4255f",
+    environment: "staging",
+    API_URL: "http://api.team2658.org:7001",
+    // API_URL: "http://0.0.0.0:7001/",
+    // API_URL: "https://api.team2658.org",
 
-      MAX_RETRIES: 3,
-      eas: {
-        projectId: "9a4e6a54-63ce-46b8-a978-ba046e0f2ae8"
+    MAX_RETRIES: 3,
+    eas: {
+      projectId: "9a4e6a54-63ce-46b8-a978-ba046e0f2ae8"
+    }
+  },
+  android: {
+    ...config.android,
+    usesCleartextTraffic: true, // Allows HTTP traffic on Android
+  },
+  ios: {
+    ...config.ios,
+    infoPlist: {
+      ...config.ios?.infoPlist,
+      NSAppTransportSecurity: {
+        NSAllowsArbitraryLoads: true, // Allows HTTP traffic on iOS
+        // Or more specifically for your domain:
+        // NSExceptionDomains: {
+        //   "api.team2658.org": {
+        //     NSExceptionAllowsInsecureHTTPLoads: true,
+        //     NSExceptionMinimumTLSVersion: "1.0"
+        //   }
+        // }
       }
-    },
-    plugins: [
-      withCustomBeaconModule,
-      [withRemoveiOSNotificationEntitlement],
-      ...(config.plugins || [])
-      
-    ],
-  });
+    }
+  },
+  plugins: [
+    withCustomBeaconModule,
+    [withRemoveiOSNotificationEntitlement],
+    ...(config.plugins || [])
+  ],
+});
